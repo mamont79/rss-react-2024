d23ee872718c95ebebc65a451ee73219cf3d0e90
Pavel Dalhou <dalhoupavel@gmail.com>
2024-08-19 00:25:47 +0200

    feat: add pagination for forms

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -3,6 +3,7 @@ import { Header } from '../../components/header/header';
 import './style.css';
 import { RootState } from '../../store/store';
 import { DataLink } from './DataLink';
+import { useState } from 'react';
 
 type UserInfoType = {
   title: string;
@@ -12,46 +13,60 @@ type UserInfoType = {
 
 export const MainPage = () => {
   const { data } = useSelector((state: RootState) => state.user);
+  const [formId, setFormId] = useState(0);
 
   const check = data.length > 1;
+  const maxId = data.length - 3;
+
+  const setNextId = () => {
+    if (formId < maxId) setFormId(formId + 1);
+  };
+  const setPrevId = () => {
+    if (formId > 0) setFormId(formId - 1);
+  };
+
   const userInfo = !check
     ? ''
     : [
         {
-          title: 'Name:',
-          dataValue: data[0].name,
-          prevDataValue: data[1].name,
-        },
-        { title: 'Age:', dataValue: data[0].age, prevDataValue: data[1].age },
-        {
-          title: 'Email:',
-          dataValue: data[0].email,
-          prevDataValue: data[1].email,
+          title: 'Name',
+          dataValue: data[formId].name,
+          prevDataValue: data[formId + 1].name,
         },
         {
-          title: 'Password:',
-          dataValue: data[0].password,
-          prevDataValue: data[1].password,
+          title: 'Age',
+          dataValue: data[formId].age,
+          prevDataValue: data[formId + 1].age,
         },
         {
-          title: 'Gender:',
-          dataValue: data[0].gender,
-          prevDataValue: data[1].gender,
+          title: 'Email',
+          dataValue: data[formId].email,
+          prevDataValue: data[formId + 1].email,
         },
         {
-          title: 'Country:',
-          dataValue: data[0].country,
-          prevDataValue: data[1].country,
+          title: 'Password',
+          dataValue: data[formId].password,
+          prevDataValue: data[formId + 1].password,
         },
         {
-          title: 'Accept Terms and Conditions:',
-          dataValue: data[0].acceptTC,
-          prevDataValue: data[1].acceptTC,
+          title: 'Gender',
+          dataValue: data[formId].gender,
+          prevDataValue: data[formId + 1].gender,
         },
         {
-          title: 'File name:',
-          dataValue: data[0].pictureName,
-          prevDataValue: data[1].pictureName,
+          title: 'Country',
+          dataValue: data[formId].country,
+          prevDataValue: data[formId + 1].country,
+        },
+        {
+          title: 'Terms and Conditions',
+          dataValue: data[formId].acceptTC,
+          prevDataValue: data[formId + 1].acceptTC,
+        },
+        {
+          title: 'File name',
+          dataValue: data[formId].pictureName,
+          prevDataValue: data[formId + 1].pictureName,
         },
       ];
 
@@ -60,13 +75,31 @@ export const MainPage = () => {
       <Header />
       <h1 className="title">USERS DATA:</h1>
       {check && (
-        <div className="user-data">
-          <DataLink
-            title={''}
-            dataValue={'Last Value'}
-            prevDataValue={'Previous Value'}
-          />
-        </div>
+        <>
+          <button
+            className="button-page"
+            onClick={setPrevId}
+            disabled={formId === 0}
+          >
+            Newer Form
+          </button>
+          <button
+            className="button-page"
+            onClick={setNextId}
+            disabled={formId === maxId}
+          >
+            Older Form
+          </button>
+          <div className="user-data">
+            <DataLink
+              title={''}
+              dataValue={
+                formId === 0 ? `Last form` : `Form was ${formId} times ago`
+              }
+              prevDataValue={`Form was ${formId + 1} times ago`}
+            />
+          </div>
+        </>
       )}
       {check ? (
         (userInfo as UserInfoType[]).map(
